5012e2b4c13b436bd73d4732f1fd4aa3466df3f7
Nicopee <gidudunicholas11@gmail.com>
2024-01-11 15:38:48 +0300

    Changed news title

--- lib/Constants/constants.dart
+++ lib/Constants/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Constants {
   static const String baseURL = 'https://newsapi.org/v2/';
-  static const String headerTitles = "Stream Tv News";
+  static const String headerTitles = "Daily news";
   static const Color primaryColor = Colors.green;
   static const int timeOut = 155000;
   static const String loadingGif = "assets/loading.json";
